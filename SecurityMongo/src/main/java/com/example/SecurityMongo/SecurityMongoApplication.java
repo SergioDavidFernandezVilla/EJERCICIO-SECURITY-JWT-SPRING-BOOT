bbda512d0a535e78fe862f5018cfb6b26375e50a
Sergio David Fernández Villa <37827861+SergioDavidFernandezVilla@users.noreply.github.com>
2024-12-23 11:54:31 -0900

    Add files via upload

---
+++ SecurityMongo/src/main/java/com/example/SecurityMongo/SecurityMongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SecurityMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityMongoApplication.class, args);
+	}
+
+}
---
+++ SecurityMongo/src/test/java/com/example/SecurityMongo/SecurityMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SecurityMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
