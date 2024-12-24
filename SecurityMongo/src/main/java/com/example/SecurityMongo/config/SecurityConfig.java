ec2715a5be21feb18c05a3963a884ecf4d94bb8b
gatocombotasfernandez <gatocombotasfernandez@gmail.com>
2024-12-24 04:56:25 +0000

    "changes at security"

--- SecurityMongo/src/main/java/com/example/SecurityMongo/config/SecurityConfig.java
+++ SecurityMongo/src/main/java/com/example/SecurityMongo/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -31,6 +32,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
             .csrf().disable()
+            .httpBasic(Customizer.withDefaults())
             .sessionManagement(sesion -> sesion.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(http -> {
                 http.requestMatchers("/auth/**").permitAll();
