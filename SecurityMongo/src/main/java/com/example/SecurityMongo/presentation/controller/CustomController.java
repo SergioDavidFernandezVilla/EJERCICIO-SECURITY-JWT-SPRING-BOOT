b5c24c5e15206c9f49c41429830303e0410b650b
gatocombotasfernandez <gatocombotasfernandez@gmail.com>
2024-12-25 07:52:59 +0000

    "restricciones"

--- SecurityMongo/src/main/java/com/example/SecurityMongo/presentation/controller/CustomController.java
+++ SecurityMongo/src/main/java/com/example/SecurityMongo/presentation/controller/CustomController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@PreAuthorize("denyAll()")
 public class CustomController {
     
     @GetMapping("/admin")
